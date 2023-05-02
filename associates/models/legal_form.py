eade724df8a1cff6067d96c7dc2e28dad882903f
pantxo-artzai <f.patissier@hodei.net>
2023-05-02 21:58:33 +0200

    [IMP] Split legal form in new module

--- associates/models/legal_form.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class LegalForm(models.Model):
-    _name = 'associates.legal_form'
-    _description = 'Legal Form'
-
-    name = fields.Char(string='Legal Form', required=True, translate=True)
-    country_id = fields.Many2one('res.country', string='Country')
-
