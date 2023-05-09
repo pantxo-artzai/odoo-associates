754a4f4febe8c38cb3e901604f94f086e2c1142f
pantxo-artzai <f.patissier@hodei.net>
2023-05-09 10:00:15 +0200

    [add] capital type

--- legal_form/models/company_extension.py
+++ legal_form/models/company_extension.py
@@ -3,6 +3,11 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    capital_type = fields.Selection([
+        ('fixed_capital', 'Fixed capital'),
+        ('open_end_investment', 'Open-end investment'),
+        ], string='Type')
     capital = fields.Float(string="Capital")
+
     legal_form_id = fields.Many2one('legal_form.legal_form', string='Legal Form')
 
