4ba1980d011cfe43a5f36b7dcb942bb1630405f1
pantxo-artzai <f.patissier@hodei.net>
2023-05-18 15:31:37 +0200

    [IMP] rename views files

--- associates/models/__init__.py
+++ associates/models/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import associate
 from . import dashboard
 from . import dividend
 from . import share
 from . import operation
+from . import associate
 from . import wizard_create_shares
 from . import wizard_update_associate
 from . import wizard_distribute_dividends
